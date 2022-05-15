05d160c56f834d9eecd7f8d9ac7d530e9bbc59ce
Eli Kanavaros <xanelik@gmail.com>
2022-05-14 18:44:22 -0700

    added course info

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,81 @@
+const Header = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>
+        {props.course} {props.name + "hello"}
+      </h1>
+    </div>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  );
+};
+
+const Content = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Number of exercises{" "}
+        {props.exercises1 + props.exercises2 + props.exercises3}
+      </p>
+    </div>
+  );
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  };
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  };
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  };
+
+  return (
+    <div>
+      <Header course={course} name={"monkey"} />
+      {/* <Part part={part1} exercises={exercises1} /> */}
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
+    </div>
+  );
+};
+
+export default App;
