8b47f08acbffa45d20b81d6219be24561154f1cb
Eli Kanavaros <xanelik@gmail.com>
2022-06-04 23:22:43 -0700

    changed courseinfo from part 1c

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,71 +1,26 @@
-const Header = (props) => {
-  return (
-    <div>
-      <h1>{props.course}</h1>
-    </div>
-  );
-};
+import { useState } from "react";
 
-const Part = (props) => {
-  return (
-    <div>
-      <p>
-        {props.parts.name} {props.parts.exercises}
-      </p>
-    </div>
-  );
-};
+const Display = (props) => <div>{props.value}</div>;
 
-const Content = (props) => {
-  return (
-    <div>
-      <Part parts={props.parts[0]} />
-      <Part parts={props.parts[1]} />
-      <Part parts={props.parts[2]} />
-    </div>
-  );
-};
-
-const Total = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <p>
-        Number of exercises{" "}
-        {props.parts[0].exercises +
-          props.parts[1].exercises +
-          props.parts[2].exercises}
-      </p>
-    </div>
-  );
-};
+const Button = (props) => (
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
 const App = () => {
-  const course = {
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-      },
-    ],
+  const [value, setValue] = useState(10);
+
+  const setToValue = (newValue) => {
+    console.log("value now", newValue);
+    setValue(newValue);
   };
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <Display value={value} />
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   );
 };
-
 export default App;
