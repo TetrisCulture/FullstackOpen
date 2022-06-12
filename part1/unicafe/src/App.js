48876a1c914e3fe7715e9f40ca0a69d94dd0530d
Eli Kanavaros <xanelik@gmail.com>
2022-06-12 14:10:00 -0700

    problem solutions for anecdotes and unicafe

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,25 +1,42 @@
-import logo from "./logo.svg";
-import "./App.css";
+import { useState } from "react";
 
-function App() {
+const Statistics = (props) => {
+  const all = props.good + props.neutral + props.bad;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <table>
+        <tr>good {props.good}</tr>
+        <tr>neutral {props.neutral}</tr>
+        <tr>bad {props.bad}</tr>
+        <tr>all {all}</tr>
+        <tr>average {all / 3}</tr>
+        <tr>positive {(props.good / all) * 100} %</tr>
+      </table>
     </div>
   );
-}
+};
+
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+
+      <h1>Statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
 
 export default App;
